8d662a6fd1cece8c3cd6e42ce516a503b7f9188b
sidneyaguirre <sidney.aguirrec@gmail.com>
2019-08-12 23:08:36 -0500

    styles details

--- src/pages/TutoryDetails.jsx
+++ src/pages/TutoryDetails.jsx
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
 
-import Tutory from "../components/Tutory"
+import "../styles/Tutor.scss";
+import Tutory from "../components/Tutory";
 
 /* it Will show the details of a tutory */
 
 class TutoryDetails extends Component {
-  state = { form: {
-    subject: 'Álgebra Lineal',
-    tutorFirstName: 'Pepito',
-    tutorLastName: 'Pérez',
-    email: 'pepito.perez@udea.edu.co',
-  } };
+  state = {
+    form: {
+      subject: "Álgebra Lineal",
+      tutorFirstName: "Pepito",
+      tutorLastName: "Pérez",
+      email: "pepito.perez@udea.edu.co"
+    }
+  };
 
   render() {
     return (
       <div>
         <div className="container">
-            <div className="row">
-                <div className="col-6">
-                    <Tutory 
-                        subject = {this.state.form.subject}
-                        tutorFirstName = {this.state.form.tutorFirstName}
-                        tutorLastName = {this.state.form.tutorLastName}
-                        email = {this.state.form.email}
-                    />
-                </div>
-            </div>
+          <div className="row">
+            <Tutory
+              subject={this.state.form.subject}
+              tutorFirstName={this.state.form.tutorFirstName}
+              tutorLastName={this.state.form.tutorLastName}
+              email={this.state.form.email}
+            />
+          </div>
         </div>
       </div>
     );
